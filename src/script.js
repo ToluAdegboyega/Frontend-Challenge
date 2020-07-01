8b6d5b9c710f5d4dd82433e0353bdd27f7f2ac29
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-07-01 15:16:58 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -25,7 +25,7 @@ private key = afcfa8996725f83ce931a01e22184e62b62e0b77
                 for(var i = 0; i<data.data.results.length; i++) {
                     var element = data.data.results[i];
                     
-                    string += " <img src'"+ element.thumbnail.path +"/portrait_fantastic."+element.thumbnail.extension  +"' />";
+                    string += " <img src'"+ element.thumbnail.path +"/portrait_small."+element.thumbnail.extension  +"' />";
 
                     string += "<h3>" +element.title + "</h3>";
 
