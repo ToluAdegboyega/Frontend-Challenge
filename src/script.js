c91fa6d5aa6b42d5e1a5b5a44380e054c80607f4
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-07-01 19:02:46 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -27,7 +27,9 @@ private key = afcfa8996725f83ce931a01e22184e62b62e0b77
                         
                         string += "<div class='cards'>";
                         string += "<ul>";
+                        string += "<a href='" +element.urls[0].url+  "' target='_blank'"
                         string += "<img src='"+element.thumbnail.path+"/portrait_uncanny."+element.thumbnail.extension+"' />";
+                        string += "</a>";
                         string += "<h3>" +element.title + "</h3>";
                         string += "<h4>" +element.dates[0].date+ "</h4>";
                         string += "</ul>";
