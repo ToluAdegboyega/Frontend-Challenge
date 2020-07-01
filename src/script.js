af78f6ceed0847b5f68c8167adb18f29ad3ce975
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-07-01 13:58:24 +0100

    js update

--- src/script.js
+++ src/script.js
@@ -2,3 +2,21 @@
 public key = 2bbafa01269795b83d2ecb54085faed4
 private key = afcfa8996725f83ce931a01e22184e62b62e0b77
  */
+
+ var marvel = {
+    render: function() {
+        var url = " http://gateway.marvel.com/v1/public/characters/1010338/comics?ts=1&apikey=2bbafa01269795b83d2ecb54085faed4&hash=fc25c4d68df68f0b6973931719a0cc20"
+        var message = document.getElementById("message");
+        $.ajax({
+            url: url,
+            type: "GET",
+            beforeSend: function() {
+                message.innerHTML = "Loading...";
+            },
+            complete: function() {
+
+            }
+        });
+    }
+ };
+ marvel.render();
